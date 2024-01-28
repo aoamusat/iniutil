4237a12b4c588d0568c6c28a6c5c14bccc137cf6
Akeem Amusat <aoamusat@gmail.com>
2024-01-29 00:20:42 +0100

    Change REPO URL

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iniutil",
-    version="1.0",
+    version="1.0.2",
     description="Python Package for parsing ini files.",
     packages=find_packages(),
     zip_safe=False,
