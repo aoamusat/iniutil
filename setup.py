05ff98b93860c138604f2558e84ee6bc4d60ce8e
Akeem Amusat <aoamusat@gmail.com>
2024-01-29 00:38:53 +0100

    Cleanup build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iniutil",
-    version="1.0.2",
+    version="1.0.3",
     description="Python Package for parsing ini files.",
     packages=find_packages(),
     zip_safe=False,
