7fa741d2bb0779a906963bb11d53527776eec161
Akeem Amusat <aoamusat@gmail.com>
2024-02-05 14:14:43 +0100

    Update pypi version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="iniutil",
-    version="1.0.3",
+    version="1.0.4",
     description="Python Package for parsing ini files.",
     packages=find_packages(),
     zip_safe=False,
